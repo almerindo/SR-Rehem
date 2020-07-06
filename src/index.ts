2d5d1d27c4a2162324b1ab9bbfe3a65e513678ee
almerindo <almerindo.rehem@gmail.com>
2020-07-05 22:45:09 -0300

    feat: add spaced repetition structure  On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   .eslintrc.json 	modified:   src/index.ts 	new file:   src/lib/Content.ts 	new file:   src/lib/SRRehem.ts 	modified:   tsconfig.json 	modified:   yarn.lock

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,56 @@
-import s from './error/teste';
+import { Content, Review, ERating } from './lib/Content';
 
-console.log('Boilerplate');
-console.log('Boilerplate');
-s('teste');
+import SRRehem from './lib/SRRehem';
+
+const vocab: Array<Content> = [
+  {
+    id: 'teste01',
+    data: {
+      fields: ['世界', 'shìjiè', 'world'],
+    },
+  },
+  {
+    id: 'teste02',
+    data: {
+      fields: ['bonjour', 'hello (good day)'],
+    },
+  },
+  {
+    id: 'teste02',
+    data: {
+      fields: ['bonjour', 'hello (good day)'],
+    },
+  },
+];
+
+const reviews: Array<Review> = [];
+
+const reviewRecord: Review = {
+  // identify which card we're reviewing
+  id: 'teste02',
+
+  // store when we reviewed it
+  whenItWasReviewd: new Date(),
+
+  // store how easy it was to remember
+  rating: ERating.GOOD,
+
+  data: {
+    fields: ['bonjour', 'hello (good day)'],
+  },
+};
+
+reviews.push(reviewRecord);
+
+const SRR = new SRRehem();
+
+vocab.forEach(content => {
+  SRR.addContent(content);
+});
+
+reviews.forEach(review => {
+  console.log(SRR.addReview(review));
+});
+
+const sumary = SRR.summary();
+console.info(sumary);
